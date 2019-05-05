47dc38dd77499ded954fa3411049fb388a704481
Mridul Saurabh Verma <saurabh.mridul@outlook.com>
2019-05-05 10:08:02 +0530

    adding models

--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Spartan.Models;
 
 namespace Spartan
 {
@@ -25,6 +26,7 @@ namespace Spartan
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ToDoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
